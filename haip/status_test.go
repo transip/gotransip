366ad41c56a5ba812489e705c6f10537b068b7a5
Reinier Schoof <reinier@skoef.nl>
2019-10-02 09:32:44 +0200

    updated status report test in haip package

--- haip/status_test.go
+++ haip/status_test.go
@@ -1,95 +1,24 @@
 package haip
 
 import (
+	"io/ioutil"
 	"net"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestParseStatusReportBody(t *testing.T) {
 
-	// example output, but shortened for brevity
-	data := []byte(`<transip>
-	<item>
-		<key xsi:type="xsd:int">80</key>
-		<value SOAP-ENC:arrayType="ns2:Map[4]" xsi:type="SOAP-ENC:Array">
-			<item xsi:type="ns2:Map">
-				<item>
-					<key xsi:type="xsd:string">vpsName</key>
-					<value xsi:type="xsd:string">example-vps</value>
-				</item>
-				<item>
-					<key xsi:type="xsd:string">ipVersions</key>
-					<value xsi:type="ns2:Map">
-						<item>
-							<key xsi:type="xsd:string">ipv4</key>
-							<value xsi:type="ns2:Map">
-								<item>
-									<key xsi:type="xsd:string">lb0.ams0</key>
-									<value xsi:type="ns2:Map">
-										<item>
-											<key xsi:type="xsd:string">loadBalancerIp</key>
-											<value xsi:type="xsd:string">1.2.3.4</value>
-										</item>
-										<item>
-											<key xsi:type="xsd:string">state</key>
-											<value xsi:type="xsd:string">up</value>
-										</item>
-										<item>
-											<key xsi:type="xsd:string">lastChange</key>
-											<value xsi:type="xsd:string">2018-08-30 20:32:33</value>
-										</item>
-										<item>
-											<key xsi:type="xsd:string">lastChangeTimestamp</key>
-											<value xsi:type="xsd:string">1535653953</value>
-										</item>
-									</value>
-								</item>
-								<item>
-									<key xsi:type="xsd:string">lb0.rtm0</key>
-									<value xsi:type="ns2:Map">
-									  innerXML is removed for brevity
-									</value>
-								</item>
-							</value>
-						</item>
-						<item>
-							<key xsi:type="xsd:string">ipv6</key>
-							<value xsi:type="ns2:Map">
-								innerXML is removed for brevity
-							</value>
-						</item>
-					</value>
-				</item>
-			</item>
-			<item xsi:type="ns2:Map">
-				<item>
-					<key xsi:type="xsd:string">vpsName</key>
-					<value xsi:type="xsd:string">example-vps2</value>
-				</item>
-				<item>
-					<key xsi:type="xsd:string">ipVersions</key>
-					<value xsi:type="ns2:Map">
-						innerXML is removed for brevity
-					</value>
-				</item>
-			</item>
-		</value>
-	</item>
-	<item>
-		<key xsi:type="xsd:int">443</key>
-		<value SOAP-ENC:arrayType="ns2:Map[4]" xsi:type="SOAP-ENC:Array">
-			innerXML removed for brevity
-		</value>
-	</item>
-</transip>`)
+	// read example status report output
+	data, err := ioutil.ReadFile("testdata/statusreportbody.xml")
+	require.NoError(t, err)
 
+	// parse status report body
 	sr, err := parseStatusReportBody(data)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	require.NoError(t, err)
 
 	// go over returned body and see if all essential structs are there
 	assert.Equal(t, 2, len(sr.PortConfiguration))
