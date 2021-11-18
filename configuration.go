cc3e259483718e32971640b82a53c7e6aa47e53e
Jordi Donadeu <jordi.donadeu@nl.team.blue>
2021-11-18 15:54:48 +0100

    Update version number

--- configuration.go
+++ configuration.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	libraryVersion  = "6.6.1"
+	libraryVersion  = "6.7.0"
 	defaultBasePath = "https://api.transip.nl/v6"
 	userAgent       = "go-client-gotransip/" + libraryVersion
 )
