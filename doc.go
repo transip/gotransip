d935ac4ffc961ec7c0cd1c5ad9f3b9f2ccf95de4
Pierre van  Horick <phorick@transip.nl>
2022-09-02 12:21:03 +0200

    lint

--- doc.go
+++ doc.go
@@ -53,6 +53,7 @@ Create a new client using a token:
 
 As tokens have a limited expiry time you can also request new tokens using the private key
 acquired from your transip control panel:
+
 	client, err := gotransip.NewClient(gotransip.ClientConfiguration{
 		AccountName:    "accountName",
 		PrivateKeyPath: "/path/to/api/private.key",
@@ -60,6 +61,7 @@ acquired from your transip control panel:
 
 We also implemented a PrivateKeyReader option, for users that want to store their key elsewhere,
 not on a filesystem but on X datastore:
+
 	file, err := os.Open("/path/to/api/private.key")
 	if err != nil {
 		panic(err.Error())
@@ -69,11 +71,11 @@ not on a filesystem but on X datastore:
 		PrivateKeyReader: file,
 	})
 
-
-TokenCache
+# TokenCache
 
 If you would like to keep a token between multiple client instantiations,
 you can provide the client with a token cache. If the file does not exist, it creates one for you
+
 	cache, err := authenticator.NewFileTokenCache("/tmp/path/to/gotransip_token_cache")
 	if err != nil {
 		panic(err.Error())
@@ -87,6 +89,7 @@ you can provide the client with a token cache. If the file does not exist, it cr
 As long as a provided TokenCache adheres to the following interface,
 the client's authenticator is able to use it. This means you can also provide
 your own token cacher: for example, one that caches to etcd
+
 	type TokenCache interface {
 		// Set will save a token by name
 		Set(key string, token jwt.Token) error
@@ -95,11 +98,12 @@ your own token cacher: for example, one that caches to etcd
 		Get(key string) (jwt.Token, error)
 	}
 
-Repositories
+# Repositories
 
 All resource calls as can be seen on https://api.transip.nl/rest/docs.html
 have been grouped in the following repositories,
 these are subpackages under the gotransip package:
+
 	availabilityzone.Repository
 	colocation.Repository
 	domain.Repository
@@ -115,10 +119,12 @@ these are subpackages under the gotransip package:
 	vps.Repository
 
 Such a repository can be initialised with a client as follows:
+
 	domainRepo := domain.Repository{Client: client}
 
 Each repository has a bunch methods you can use to call get/modify/update resources in
 that specific subpackage. For example, here we get a list of domains from a transip account:
+
 	domains, err := domainRepo.GetAll()
 */
 package gotransip
